2a2f0be2d356b6ec44d5301d33a4aab69d066614
Sergii Chub <s.chub@exlabs.pl>
2019-09-26 18:38:57 +0200

    [CAZ-1138] Create Non-Uk page

---
+++ spec/requests/vehicles_controller/non_uk_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'VehiclesController - GET #non_uk', type: :request do
+  subject { get non_uk_vehicles_path }
+
+  context 'with VRN in session' do
+    before { add_vrn_to_session }
+
+    it 'returns a success response' do
+      subject
+      expect(response).to be_successful
+    end
+  end
+end
