daaec239b4712e6806334a2747fc758cd781bdc3
Wojtek Mikusek <miqs1992@gmail.com>
2019-10-30 08:47:15 +0100

    [NT] Change same site policy

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,4 +2,5 @@
 
 Rails.application.config.session_store :cookie_store,
                                        key: '_caz-payments_session',
-                                       same_site: :strict
+                                       # Replace SameSite policy to be able to load session after redirect from GOV.UK Pay
+                                       same_site: :lax # :strict
