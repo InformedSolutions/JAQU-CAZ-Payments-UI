64949fba820c46ae27c515273e904d571cf07977
Alice King <alice.king@informed.com>
2020-01-17 12:53:45 +0000

    fix static code analysis error

--- app/api_wrapper/payments_api.rb
+++ app/api_wrapper/payments_api.rb
@@ -80,7 +80,7 @@ class PaymentsApi < BaseApi
     #
     # ==== Example
     #
-    #    PaymentsApi.payment_status(payment_id: '86b64512-154c-4033-a64d-92e8ed19275f', 
+    #    PaymentsApi.payment_status(payment_id: '86b64512-154c-4033-a64d-92e8ed19275f',
     #                               caz_name: 'Leeds')
     #
     # ==== Result
