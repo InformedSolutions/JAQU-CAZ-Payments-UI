c28d88692779b8c2de6d680ae19e833f089f6762
Alice King <alice.king@informed.com>
2020-02-05 13:46:25 +0000

    refactor to move date generation out to new function

--- features/step_definitions/charges_steps.rb
+++ features/step_definitions/charges_steps.rb
@@ -113,7 +113,7 @@ end
 
 Given('I am on the dates page with all charges paid') do
   add_vehicle_details_to_session
-  mock_paid_dates(dates: ((Date.current - 6.days)..(Date.current + 6.days)).map(&:to_s))
+  mock_paid_dates(paid_period)
   visit select_daily_date_dates_path
 end
 
@@ -138,4 +138,11 @@ Given('I am on the pick weekly dates page with no passes available to buy') do
   add_weekly_vehicle_details_to_session
   mock_paid_dates(dates: ((Date.current - 6.days)..(Date.current + 6.days)).map(&:to_s))
   visit select_weekly_date_dates_path
+end
+private
+
+def paid_period
+  {
+    dates: ((Date.current - 6.days)..(Date.current + 6.days)).map(&:to_s)
+  }
 end
