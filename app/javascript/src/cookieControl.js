9272aefaacfb3ecb9189c7275ada38bcaf9c91d9
Sergii Chub <s.chub@exlabs.pl>
2020-04-01 17:34:55 +0200

    revert [CAZ-2242] Set secure flag for ControlCookie (#225)

--- app/javascript/src/cookieControl.js
+++ app/javascript/src/cookieControl.js
@@ -3,6 +3,4 @@ export default function () {
   var style = document.createElement('style');
   style.innerHTML = onPageStyle;
   document.head.appendChild(style);
-  // Set cookie secure flag for CookieControl
-  document.cookie = "tagname = CookieControl;Secure"
 }
