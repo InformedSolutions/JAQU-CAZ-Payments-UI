6ef30813a4c7d18bdc09cae385393f36e73e3c7b
Sergii Chub <s.chub@exlabs.pl>
2020-10-12 14:27:24 +0200

    Bump ruby and libs

--- spec/support/expectations/date_string.rb
+++ spec/support/expectations/date_string.rb
@@ -4,7 +4,7 @@ require 'rspec/expectations'
 
 RSpec::Matchers.define :be_a_date_string do
   match do |actual|
-    actual.to_date.class == Date
+    actual.to_date.instance_of?(Date)
   rescue ArgumentError
     false
   end
