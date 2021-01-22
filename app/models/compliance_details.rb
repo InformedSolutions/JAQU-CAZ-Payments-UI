107bf9740c018675e52292125f7d52e2e20f8758
dratka <d.ratka@exlabs.pl>
2021-01-22 13:59:13 +0100

    [CAZB-3869] PHGV discount page

--- app/models/compliance_details.rb
+++ app/models/compliance_details.rb
@@ -84,6 +84,13 @@ class ComplianceDetails
     zone_links['fleet']
   end
 
+  # Returns information if pghvDiscount is Available for DVLA vehicle.
+  def phgv_discount_available?
+    return false if non_dvla
+
+    dvla_compliance_data['phgvDiscountAvailable']
+  end
+
   private
 
   # Attributes used to perform the backend call
@@ -104,13 +111,18 @@ class ComplianceDetails
       if non_dvla
         non_dvla_compliance_data
       else
-        dvla_compliance_data
+        dvla_compliance_data_outcome
       end.first.deep_transform_keys { |key| key.underscore.to_sym }
   end
 
   # Get compliance data for DVLA registered vehicle
   def dvla_compliance_data
-    ComplianceCheckerApi.vehicle_compliance(vrn, [zone_id])['complianceOutcomes']
+    @dvla_compliance_data ||= ComplianceCheckerApi.vehicle_compliance(vrn, [zone_id])
+  end
+
+  # Get compliance data for DVLA registered vehicle
+  def dvla_compliance_data_outcome
+    dvla_compliance_data['complianceOutcomes']
   end
 
   # Get compliance data for non-DVLA registered vehicle
