7f59a31a13e5418ec88340077b2a731267ee7f30
James Proctor <43146680+proctorj@users.noreply.github.com>
2021-05-18 12:33:10 +0100

    [CAZB-4854] Allow multiple hosts (#653)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -181,7 +181,7 @@ class ApplicationController < ActionController::Base # rubocop:disable Metrics/C
   # Checks if hosts were not manipulated
   # :nocov:
   def validate_host_headers!
-    Security::HostHeaderValidator.call(request: request, allowed_host: Rails.configuration.x.host)
+    Security::HostHeaderValidator.call(request: request, allowed_hosts: Rails.configuration.x.host)
   end
   # :nocov:
 
