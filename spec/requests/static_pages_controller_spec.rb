70a81319ae98e96411b4962356bf5587adc43beb
Dawid <d.ratka@exlabs.pl>
2021-07-07 10:45:49 +0200

    [CAZB-5104] Update service to flag Invalid Host Headers with 403 error (#709)
    * [CAZB-5104] update service to flag Invalid Host Headers with 403 error
    
    * add test

--- spec/requests/static_pages_controller_spec.rb
+++ spec/requests/static_pages_controller_spec.rb
@@ -22,4 +22,21 @@ describe StaticPagesController, type: :request do
 
     it_behaves_like 'a static page'
   end
+
+  context 'when service call returns `InvalidHostException`' do
+    subject { get privacy_notice_path }
+
+    before do
+      allow(CazDataProvider).to receive(:new).and_raise(InvalidHostException)
+      subject
+    end
+
+    it 'renders the service unavailable page' do
+      expect(response).to render_template(:service_unavailable)
+    end
+
+    it 'returns a :forbidden response' do
+      expect(response).to have_http_status(:forbidden)
+    end
+  end
 end
